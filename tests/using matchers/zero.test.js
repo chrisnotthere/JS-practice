8b3b5d70452cad89c8241e3b56ae470669dd6d35
Chris <mr.chrisnash@gmail.com>
2021-09-01 13:29:58 -0700

    tests

---
+++ tests/using matchers/zero.test.js
@@ -0,0 +1,9 @@
+test('zero', () => {
+  const z = 0;
+  expect(z).not.toBeNull();
+  expect(z).toBeDefined();
+  expect(z).not.toBeUndefined();
+  expect(z).not.toBeTruthy();
+  expect(z).toBeFalsy();
+});
+
